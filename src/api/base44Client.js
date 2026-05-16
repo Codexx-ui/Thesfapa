847d4f953e9cfe77d999e6c84b4a0ff36701c62e
Codexx <tsafrass@gmail.com>
2026-05-16 13:11:31 +0300

    Fix: Add App ID and API Key for backend integration

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -5,10 +5,8 @@ const { appId, token, functionsVersion, appBaseUrl } = appParams;
 
 //Create a client with authentication required
 export const base44 = createClient({
-  appId,
-  token,
-  functionsVersion,
-  serverUrl: '',
-  requiresAuth: false,
-  appBaseUrl
+  appId: "6a02d2983989447500838a5e",
+  headers: {
+    "api_key": "9cfe17cba67b408e943bd4056e593b9d"
+  }
 });
